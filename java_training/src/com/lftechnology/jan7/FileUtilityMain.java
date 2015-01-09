ae761778730aed6a8dd340f035770983b110a65f
Nerisha <nerisachitrakar@lftechnology.com>
2015-01-09 11:25:16 +0545

    user specific home directory added

--- java_training/src/com/lftechnology/jan7/FileUtilityMain.java
+++ java_training/src/com/lftechnology/jan7/FileUtilityMain.java
@@ -11,12 +11,17 @@ import java.util.logging.Logger;
  * 
  */
 public class FileUtilityMain {
-	private static final Logger LOGGER = Logger.getLogger(FileUtility.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(FileUtilityMain.class.getName());
 
 	public static void main(String[] args) {
+		final String PATH_TO_HOME = System.getProperty("user.home");
+		final String PATH_TO_DIRECTORY = "/javatest/";
+		final String NEW_FILE = "abc.txt";
+		final String NEW_DIRECTORY = "newdir";
+		final String FILE_TO_READ = "in.txt";
 		FileUtility file = new FileUtility();
-		file.createAFile("/home/nerisa/javatest/abc.txt");
-		file.makeDirectory("/home/nerisa/javatest/newdir");
+		file.createAFile(PATH_TO_HOME + PATH_TO_DIRECTORY + NEW_FILE);
+		file.makeDirectory(PATH_TO_HOME + PATH_TO_DIRECTORY + NEW_DIRECTORY);
 		// rename a file by accepting the file name from command line arguments
 		if (args.length == 2) {
 			Path oldFile = Paths.get(args[0]);
@@ -24,7 +29,7 @@ public class FileUtilityMain {
 		} else {
 			LOGGER.severe("File names missing");
 		}
-		file.readFile("/home/nerisa/javatest/in.txt");
+		file.readFile(PATH_TO_HOME + PATH_TO_DIRECTORY + FILE_TO_READ);
 		file.displayData();
 	}
 
