8325960413a8d3fdf2ce8e756ff340c886b71c24
Nerisha <nerisachitrakar@lftechnology.com>
2015-01-14 10:13:02 +0545

    unnecessary methods removed

--- java_training/src/com/lftechnology/jan12/ArrayListManipulator.java
+++ java_training/src/com/lftechnology/jan12/ArrayListManipulator.java
@@ -7,59 +7,42 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * This class explores the {@link ArrayList} class has methods to
- * <ol>
- * <li>Add elements to its instance variable</li>
- * <li>Shuffle the elements of the instance variable and retrieve a specified length of sublist</li>
- * <li>Sort the instance variable</li>
- * <li>Display the instance variable</li>
- * </ol>
+ * Create an array list with 49 Integer objects containing the numbers 1 through 49. It then randomly shuffles the list and selects the
+ * first 6 values from the shuffled list. Finally, it sorts the selected values and prints them out. Hint: Use ArrayList,
+ * Collections.shuffle(), ArrayList.subList(), and Collection.sort().
  * 
- * @author nerisa
+ * @author Nerisa Chitrakar nerisachitrakar@lftechnology.com
  * 
  */
-class ArrayListManipulator {
-	public List<Integer> list = new ArrayList<Integer>();
+public class ArrayListManipulator {
+	private static List<Integer> list = new ArrayList<Integer>();
 	private static final Logger LOG = Logger.getLogger(ArrayListManipulator.class.getName());
 
+	public static void main(String[] args) {
+		for (int i = 1; i < 50; i++) {
+			list.add(i);
+		}
+		LOG.log(Level.INFO, "Main list:");
+		displayList(list);
+		Collections.shuffle(list);
+		LOG.log(Level.INFO, "Shuffled list:");
+		displayList(list);
+		List<Integer> childList = list.subList(0, 6);
+		LOG.log(Level.INFO, "Extracted list:");
+		displayList(childList);
+		Collections.sort(childList);
+		LOG.log(Level.INFO, "Sorted child list:");
+		displayList(childList);
+	}
+
 	/**
-	 * This method adds an element to the instance variable.
+	 * This method displays the list in one line
 	 * 
-	 * @author nerisa
-	 * @param element
+	 * @author Nerisa Chitrakar nerisachitrakar@lftechnology.com
+	 * @param list
+	 *            {@link List} to be displayed
 	 */
-	public void addElement(int element) {
-		list.add(element);
-	}
-
-	/**
-	 * This method shuffles the instance variable and returns the specified length sublist of the instance variable
-	 * 
-	 * @author nerisa
-	 * @param fromIndex
-	 *            specifies the starting index to extract the sublist
-	 * @param toIndex
-	 *            specifies the ending index of the sublist
-	 * @return an instance of the class with the sublist as its instance variable
-	 */
-	public ArrayListManipulator shuffleAndRetrieve(int fromIndex, int toIndex) {
-		ArrayListManipulator subList = new ArrayListManipulator();
-		Collections.shuffle(this.list);
-		subList.list = list.subList(fromIndex, toIndex);
-		return subList;
-	}
-
-	/**
-	 * @author nerisa
-	 */
-	public void sort() {
-		Collections.sort(list);
-	}
-
-	/**
-	 * @author nerisa
-	 */
-	public void display() {
+	private static void displayList(List<Integer> list) {
 		StringBuilder intList = new StringBuilder();
 		for (Integer num : list) {
 			intList.append(num + " ");
